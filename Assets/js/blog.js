0dea73a604ffb7b9cd74ff777dd91d8c7e6e7467
TheR16H <rashawn5hall@gmail.com>
2024-05-16 15:22:45 -0400

    more fixes

--- Assets/js/blog.js
+++ Assets/js/blog.js
@@ -1,5 +1,40 @@
- const storedPosts = JSON.parse(localStorage.getItem('blog'));
- console.log("Stored Data: ", storedPosts);
+
+
+// Retrieves data from local storage
+blogsArray = JSON.parse(localStorage.getItem('blog'));
+
+// accesses the elements after retrieving the data
+const blogForm = document.getElementById('blogForm');
+const authorEl = document.getElementById('author');
+const titleEl = document.getElementById('title');
+const contentEl = document.getElementById('content');
+
+console.log("Stored Data: ", blogsArray)
+
+const postContainer = document.getElementById('postContainer');
+
+blogsArray.forEach(blog => {
+    const postElement = document.createElement('div');
+    
+    const authorEl = document.createElement('h3');
+    authorEl.textContent = blog.name;
+
+    const titleEl = document.createElement('h4');
+    titleEl.textContent = blog.title;
+
+    const contentEl = document.createElement('p');
+    contentEl.textContent = blog.content;
+
+    postElement.append(authorEl);
+    postElement.append(titleEl);
+    postElement.append(contentEl);
+
+    postContainer.append(postElement);
+    console.log("The appends are working...probably")
+
+});
+
+
 
 // const posts = [
 //     {
@@ -15,24 +50,24 @@
 // ]; 
 
 // function generateBlogPostElement(post){
-//     const postElement = document.createElement('div');
+//     const postEl = document.createElement('div');
 
-//     const authorElement = document.createElement('h3');
-//     authorElement.textContent = post.author;
+//     const authorEl = document.createElement('h3');
+//     authorEl.textContent = post.author;
 
-//     const titleElement = document.createElement('h4');
-//     titleElement.textContent = post.title;
+//     const titleEl = document.createElement('h4');
+//     titleEl.textContent = post.title;
 
-//     const contentElement = document.createElement('p');
-//     contentElement.textContent=post.content;
+//     const contentEl = document.createElement('p');
+//     contentEl.textContent=post.content;
 
-//     postElement.appendChild(authorElement);
-//     postElement.appendChild(titleElement);
-//     postElement.appendChild(contentElement);
+//     postElement.appendChild(authorEl);
+//     postElement.appendChild(titleEl);
+//     postEl.appendChild(contentEl);
     
-//     return postElement;
+//     return postEl;
 // };
-// const postsContainer = document.getElementById('postsContainer');
+
 
 // storedPosts.forEach(post => {
 //     const postElement = generateBlogPostElement(post);
@@ -58,9 +93,9 @@
 //     const contentElement = document.createElement('p');
 //     contentElement.textContent = post.content;
 
-//     postElement.appendChild(authorElement);
-//     postElement.appendChild(titleElement);
-//     postElement.appendChild(contentElement);
+//     postElement.appendChild(authorEl);
+//     postElement.appendChild(titleEl);
+//     postElement.appendChild(contentEl);
 
 //     postBox.appendChild(postElement);
 
@@ -74,3 +109,27 @@
 //     postsContainer.appendChild(postElement);
 // });
 // localStorage.setItem('blogPosts', JSON.stringify(posts));
+
+
+// if (!storedPosts) {
+//     let storedPosts = JSON.parse(localStorage.getItem('blog'));
+// }
+
+// // Check if postsContainer is already declared
+// if (!postsContainer) {
+//     let postsContainer = document.getElementById('posts-container'); // Assuming you have a container in your HTML with id 'posts-container'
+// }
+
+// if (storedPosts && postsContainer) {
+//     storedPosts.forEach(post => {
+//         const postElement = generateBlogPostElement(post);
+//         postsContainer.appendChild(postElement);
+//     });
+// }
+// const dataContainer = document.getElementById('postContainer'); 
+// // Append data from array1
+// blogsArray.forEach(item => {
+//     const itemElement = document.createElement('p');
+//     itemElement.textContent = item;
+//     dataContainer.appendChild(itemElement); 
+// });
